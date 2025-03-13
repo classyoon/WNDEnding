8546bdb2a9c7d6daccd898f83110f6819a561d50
classyoon <96508736+classyoon@users.noreply.github.com>
2025-03-13 15:15:15 -0400

    Initial Commit

---
+++ WNDEnding/WNDEndingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WNDEndingApp.swift
+//  WNDEnding
+//
+//  Created by Conner Yoon on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct WNDEndingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WNDEndingTests/WNDEndingTests.swift
@@ -0,0 +1,17 @@
+//
+//  WNDEndingTests.swift
+//  WNDEndingTests
+//
+//  Created by Conner Yoon on 3/13/25.
+//
+
+import Testing
+@testable import WNDEnding
+
+struct WNDEndingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
